aba2e8dec508155727c826d3d7c8bf6af95f5a38
Vijay Pasam <vijaysk.pasam@gmail.com>
2022-10-12 20:27:49 -0400

    first commit

---
+++ Cambrain_Foodos/Data/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Cambrain_Foodos.Data
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
---
+++ Cambrain_Foodos/Data/UserService.cs
@@ -0,0 +1,22 @@
+﻿namespace Cambrain_Foodos.Data
+{
+    public class UserService
+    {
+        private static List<User> mockdb = new List<User>()
+        {
+            new User(){Name="Vijay", Email="vijay@gmail.com", PhoneNumber="6475545101", Address="30 tuxdeo", UserName="vj116", Password="4132"}
+        };
+
+        public async Task<bool> AddUser(User newUser)
+        {
+            mockdb.Add(newUser);
+            return true;
+        }
+
+        public async Task<List<User>> GetAllUsers()
+        {
+            return mockdb;
+        }
+
+    }
+}
